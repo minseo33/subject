f00fe03c840f535fa9705003ffb1f4574bdedd2e
kth-elonsoft <kth@elonsoft.co.kr>
2023-09-19 11:05:12 +0900

    RestController 추가

---
+++ src/main/java/com/intern/backend/backend/endpoint/api/IndexApiController.java
@@ -0,0 +1,13 @@
+package com.intern.backend.backend.endpoint.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexApiController {
+
+    @GetMapping("/api")
+    public String indexApi() {
+        return "index";
+    }
+}
